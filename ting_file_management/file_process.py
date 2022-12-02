962f0063d1c1bb479f94d4c1e893ff24d6de11d1
DeividBorges93 <deividmborges@gmail.com>
2022-12-02 11:49:48 -0300

    feat(file_process): :sparkles: implementa funções process e remove
    process: pega as informações de txt_importer e gera um dicionário que será armazenado dentro da Queue
    
    remove: remove o primeiro arquivo processado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,26 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    file = txt_importer(path_file)
+    fileInfos = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(fileInfos)
+    return print(fileInfos, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance or len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    path = instance.dequeue()["nome_do_arquivo"]
+    return sys.stdout.write(f"Arquivo {path} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
